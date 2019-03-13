fb93e88b8f3681742a267a9d7ea03232a687f84a
yangziwen <yangziwen@meituan.com>
2019-03-13 15:52:51 +0800

    add command

--- src/main/java/io/github/yangziwen/cookieproxy/Main.java
+++ src/main/java/io/github/yangziwen/cookieproxy/Main.java
@@ -1,10 +1,51 @@
 package io.github.yangziwen.cookieproxy;
 
+import java.util.Objects;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+import io.github.yangziwen.cookieproxy.command.Command;
+import io.github.yangziwen.cookieproxy.command.ServerCommand;
+
 public class Main {
 
     public static void main(String[] args) {
-        Server.run("0.0.0.0", 8415);
-        System.out.println("cookie-proxy started");
+
+        JCommander commander = new JCommander();
+
+        Command[] commands = { new ServerCommand() };
+
+        for (Command command : commands) {
+            commander.addCommand(command.name(), command);
+        }
+
+        if (ArrayUtils.isEmpty(args)) {
+            commander.usage();
+            return;
+        }
+
+        String commandName = parseArgs(commander, args).getParsedCommand();
+
+        for (Command command : commands) {
+            if (Objects.equals(commandName, command.name())) {
+                command.invoke(commander);
+                return;
+            }
+        }
+        System.err.println("Invalid command!");
+    }
+
+    private static JCommander parseArgs(JCommander commander, String[] args) {
+        try {
+            commander.parse(args);
+        } catch (ParameterException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        return commander;
     }
 
 }
